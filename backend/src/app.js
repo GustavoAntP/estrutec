3577a7f09c32bb4a0ae2bec2a4c3ab4807746ce0
Gusta <gustavo_a_pereira@estudante.sc.senai.br>
2026-08-18 16:01:59 -0300

    feat: configura conexão com PostgreSQL e Sequelize

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const sequelize = require("./configuracoes/banco");
+
 const app = express();
 
 app.use(cors());
@@ -8,12 +10,25 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
-    mensagem: "API do Estrutec funcionando!"
+    mensagem: "API do Estrutec funcionando!",
   });
 });
 
 const PORTA = 3001;
 
-app.listen(PORTA, () => {
-  console.log(`Servidor Estrutec rodando na porta ${PORTA}`);
-});
+async function iniciarServidor() {
+  try {
+    await sequelize.authenticate();
+
+    console.log("Conexão com o PostgreSQL realizada com sucesso!");
+
+    app.listen(PORTA, () => {
+      console.log(`Servidor Estrutec rodando na porta ${PORTA}`);
+    });
+  } catch (erro) {
+    console.error("Erro ao conectar com o PostgreSQL:");
+    console.error(erro);
+  }
+}
+
+iniciarServidor();
